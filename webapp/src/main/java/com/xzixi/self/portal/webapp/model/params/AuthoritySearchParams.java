86e6fff59e3f82a5f42e526a3171a9ae4ba3aa51
xuelingkang <xuelingkang@163.com>
2019-12-24 23:11:35 +0800

    细节优化

--- webapp/src/main/java/com/xzixi/self/portal/webapp/model/params/AuthoritySearchParams.java
+++ webapp/src/main/java/com/xzixi/self/portal/webapp/model/params/AuthoritySearchParams.java
@@ -3,10 +3,12 @@ package com.xzixi.self.portal.webapp.model.params;
 import com.xzixi.self.portal.webapp.framework.model.BaseSearchParams;
 import com.xzixi.self.portal.webapp.model.po.Authority;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
 
 /**
  * @author 薛凌康
  */
+@Data
 @ApiModel(description = "权限查询参数")
 public class AuthoritySearchParams extends BaseSearchParams<Authority> {
 }
