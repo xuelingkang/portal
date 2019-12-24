a89fb78bddfffc34c9997987983d59d78d3f992f
xuelingkang <xuelingkang@163.com>
2019-12-24 21:14:58 +0800

    swagger2扩展

---
+++ extension/swagger2/src/main/java/com/xzixi/self/portal/extension/swagger2/annotation/IgnoreSwagger2Parameter.java
@@ -0,0 +1,9 @@
+package com.xzixi.self.portal.extension.swagger2.annotation;
+
+/**
+ * 设置swagger2文档需要忽略的参数
+ *
+ * @author 薛凌康
+ */
+public @interface IgnoreSwagger2Parameter {
+}
---
+++ extension/swagger2/src/main/java/com/xzixi/self/portal/extension/swagger2/util/FieldUtil.java
@@ -0,0 +1,31 @@
+package com.xzixi.self.portal.extension.swagger2.util;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author 薛凌康
+ */
+public class FieldUtil {
+
+    /**
+     * 按照名称获取字段
+     *
+     * @param cls 类对象
+     * @param name 字段名
+     * @return 目标字段
+     */
+    public static Field getDeclaredField(Class<?> cls, String name) {
+        Field field = null;
+        try {
+            if (cls==null) {
+                return null;
+            }
+            field = cls.getDeclaredField(name);
+        } catch (NoSuchFieldException e) {
+            field = getDeclaredField(cls.getSuperclass(), name);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        return field;
+    }
+}
