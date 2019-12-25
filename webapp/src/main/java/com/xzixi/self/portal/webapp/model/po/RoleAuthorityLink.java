536be84c5e791f9a1a7e9981d8a63950cfc171a1
xuelingkang <xuelingkang@163.com>
2019-12-25 22:34:55 +0800

    修改角色权限接口

--- webapp/src/main/java/com/xzixi/self/portal/webapp/model/po/RoleAuthorityLink.java
+++ webapp/src/main/java/com/xzixi/self/portal/webapp/model/po/RoleAuthorityLink.java
@@ -2,11 +2,13 @@ package com.xzixi.self.portal.webapp.model.po;
 
 import com.xzixi.self.portal.webapp.framework.model.BaseModel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author 薛凌康
  */
 @Data
+@NoArgsConstructor
 public class RoleAuthorityLink extends BaseModel {
 
     private static final long serialVersionUID = 1L;
@@ -16,4 +18,9 @@ public class RoleAuthorityLink extends BaseModel {
     private Integer roleId;
 
     private Integer authorityId;
+
+    public RoleAuthorityLink(Integer roleId, Integer authorityId) {
+        this.roleId = roleId;
+        this.authorityId = authorityId;
+    }
 }
