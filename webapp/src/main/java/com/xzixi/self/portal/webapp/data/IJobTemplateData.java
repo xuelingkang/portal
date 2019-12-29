0d91b3d37d80aa88a140df1323156538718fb519
xuelingkang <xuelingkang@163.com>
2019-12-30 00:07:03 +0800

    bug修正

--- webapp/src/main/java/com/xzixi/self/portal/webapp/data/IJobTemplateData.java
+++ webapp/src/main/java/com/xzixi/self/portal/webapp/data/IJobTemplateData.java
@@ -7,4 +7,7 @@ import com.xzixi.self.portal.webapp.model.po.JobTemplate;
  * @author 薛凌康
  */
 public interface IJobTemplateData extends IBaseData<JobTemplate> {
+
+    String BASE_CACHE_NAME = "jobTemplate:base";
+    String CASUAL_CACHE_NAME = "jobTemplate:casual";
 }
--- webapp/src/main/java/com/xzixi/self/portal/webapp/data/IJobTemplateParameterData.java
+++ webapp/src/main/java/com/xzixi/self/portal/webapp/data/IJobTemplateParameterData.java
@@ -7,4 +7,7 @@ import com.xzixi.self.portal.webapp.model.po.JobTemplateParameter;
  * @author 薛凌康
  */
 public interface IJobTemplateParameterData extends IBaseData<JobTemplateParameter> {
+
+    String BASE_CACHE_NAME = "jobTemplateParameter:base";
+    String CASUAL_CACHE_NAME = "jobTemplateParameter:casual";
 }
