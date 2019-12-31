18b9eae95b135b66fd07d6256fe754e6a0064f36
xuelingkang <xuelingkang@163.com>
2019-12-31 17:38:48 +0800

    去掉测试代码

--- webapp/src/main/java/com/xzixi/self/portal/webapp/model/params/AuthoritySearchParams.java
+++ webapp/src/main/java/com/xzixi/self/portal/webapp/model/params/AuthoritySearchParams.java
@@ -1,7 +1,6 @@
 package com.xzixi.self.portal.webapp.model.params;
 
 import com.xzixi.self.portal.webapp.framework.model.BaseSearchParams;
-import com.xzixi.self.portal.webapp.framework.model.annotation.Like;
 import com.xzixi.self.portal.webapp.model.po.Authority;
 import io.swagger.annotations.ApiModel;
 import lombok.Data;
@@ -12,10 +11,4 @@ import lombok.Data;
 @Data
 @ApiModel(description = "权限查询参数")
 public class AuthoritySearchParams extends BaseSearchParams<Authority> {
-
-    /**
-     * TODO 测试用，用完删除
-     */
-    @Like("pattern")
-    private String patternLike;
 }
