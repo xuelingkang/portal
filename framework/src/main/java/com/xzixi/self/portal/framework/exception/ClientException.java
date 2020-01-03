dae325ed9707307b11c977a44b6fae06dc7cee08
xuelingkang <xuelingkang@163.com>
2020-01-03 18:59:29 +0800

    细节优化

--- framework/src/main/java/com/xzixi/self/portal/framework/exception/ClientException.java
+++ framework/src/main/java/com/xzixi/self/portal/framework/exception/ClientException.java
@@ -12,28 +12,8 @@ public class ClientException extends RuntimeException {
 
     private int status;
 
-    public ClientException(int status) {
-        super();
-        this.status = status;
-    }
-
     public ClientException(int status, String message) {
         super(message);
-        this.status = status;
-    }
-
-    public ClientException(int status, String message, Throwable cause) {
-        super(message, cause);
-        this.status = status;
-    }
-
-    public ClientException(int status, Throwable cause) {
-        super(cause);
-        this.status = status;
-    }
-
-    protected ClientException(int status, String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
         this.status = status;
     }
 }
