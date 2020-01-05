d52c6597f134abfd30201f20618a316f66be0b56
xuelingkang <xuelingkang@163.com>
2020-01-05 22:57:47 +0800

    细节优化

--- webapp/src/main/java/com/xzixi/self/portal/webapp/constant/Swagger2Constant.java
+++ webapp/src/main/java/com/xzixi/self/portal/webapp/constant/Swagger2Constant.java
@@ -8,7 +8,7 @@ public interface Swagger2Constant {
     String AUTHENTICATION_API_FILTER_NAME = "swaggerFilter";
     String AUTHENTICATION_PARAMETER_NAME = "Authorization";
     String AUTHENTICATION_PARAMETER_TYPE = "header";
-    String AUTHENTICATION_EXCLUDE_URL_REG = "^(?!/login).*$";
+    String AUTHENTICATION_EXCLUDE_URL_REG = "^(?!(/login|/website/user)).*$";
     String AUTHENTICATION_SCOPE_NAME = "global";
     String AUTHENTICATION_SCOPE_DESCRIPTION = "全局";
     String AUTHENTICATION_TEMPLATE = "/authentication.json";
