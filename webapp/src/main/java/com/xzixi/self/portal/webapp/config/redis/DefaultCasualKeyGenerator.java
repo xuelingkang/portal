e30b389af1ed0f4d633558a3431ecd1c935cadd6
xuelingkang <xuelingkang@163.com>
2020-01-10 00:57:33 +0800

    修复QueryWrapper参数生成的key某些情况下无法命中缓存的bug

--- webapp/src/main/java/com/xzixi/self/portal/webapp/config/redis/DefaultCasualKeyGenerator.java
+++ webapp/src/main/java/com/xzixi/self/portal/webapp/config/redis/DefaultCasualKeyGenerator.java
@@ -1,7 +1,7 @@
 package com.xzixi.self.portal.webapp.config.redis;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.xzixi.self.portal.webapp.util.TypeUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.cache.interceptor.KeyGenerator;
@@ -32,17 +32,32 @@ public class DefaultCasualKeyGenerator implements KeyGenerator {
     private String writeParams(Object... params) {
         if (params.length > 0) {
             return StringUtils.join(Arrays.stream(params).map(param -> {
+                if (param == null) {
+                    return "null";
+                }
                 if (TypeUtil.isSimpleValueType(param.getClass())) {
                     return param;
-                } else {
-                    return DigestUtils.md5DigestAsHex(toJson(param).getBytes(StandardCharsets.UTF_8));
                 }
+                /*
+                 * QueryWrapper类型参数如果调用了in等链式方法，
+                 * 如queryWrapper.in("id", ids)
+                 * 虽然每次设置相同的参数，最终生成的缓存key也相同，但是无法命中缓存，
+                 * 暂时使用这种方式可以解决，以后再调查真相
+                 */
+                if (QueryWrapper.class.isAssignableFrom(param.getClass())) {
+                    QueryWrapper<?> queryWrapper = (QueryWrapper<?>) param;
+                    String info = String.format("entity=%s,select=%s,sqlSegment=%s,nameValue=%s",
+                            toJson(queryWrapper.getEntity()), queryWrapper.getSqlSelect(),
+                            queryWrapper.getSqlSegment(), queryWrapper.getParamNameValuePairs());
+                    return DigestUtils.md5DigestAsHex(info.getBytes(StandardCharsets.UTF_8));
+                }
+                return DigestUtils.md5DigestAsHex(toJson(param).getBytes(StandardCharsets.UTF_8));
             }).toArray(), PARAM_SEPARATOR);
         }
         return EMPTY_PARAM;
     }
 
     private String toJson(Object obj) {
-        return JSON.toJSONString(obj, SerializerFeature.WriteMapNullValue);
+        return JSON.toJSONString(obj);
     }
 }
