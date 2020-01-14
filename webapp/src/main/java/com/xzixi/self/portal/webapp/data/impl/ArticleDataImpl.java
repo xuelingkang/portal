cc2dd9ec1b103c0786118e80677cdd9f49b0317e
xuelingkang <xuelingkang@163.com>
2020-01-14 23:36:51 +0800

    继承ElasticsearchDataImpl

--- webapp/src/main/java/com/xzixi/self/portal/webapp/data/impl/ArticleDataImpl.java
+++ webapp/src/main/java/com/xzixi/self/portal/webapp/data/impl/ArticleDataImpl.java
@@ -1,7 +1,6 @@
 package com.xzixi.self.portal.webapp.data.impl;
 
-import com.xzixi.self.portal.enhance.annotation.CacheEnhance;
-import com.xzixi.self.portal.framework.data.impl.MybatisPlusDataImpl;
+import com.xzixi.self.portal.framework.data.impl.ElasticsearchDataImpl;
 import com.xzixi.self.portal.webapp.data.IArticleData;
 import com.xzixi.self.portal.webapp.mapper.ArticleMapper;
 import com.xzixi.self.portal.webapp.model.po.Article;
@@ -11,6 +10,5 @@ import org.springframework.stereotype.Service;
  * @author 薛凌康
  */
 @Service
-@CacheEnhance(baseCacheName = "article:base", casualCacheName = "article:casual")
-public class ArticleDataImpl extends MybatisPlusDataImpl<ArticleMapper, Article> implements IArticleData {
+public class ArticleDataImpl extends ElasticsearchDataImpl<ArticleMapper, Article> implements IArticleData {
 }
