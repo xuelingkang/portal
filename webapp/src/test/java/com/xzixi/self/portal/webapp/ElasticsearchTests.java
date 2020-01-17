cf4a556834018db81aa017d8c190ce64c989d692
xuelingkang <xuelingkang@163.com>
2020-01-17 09:34:15 +0800

    单元测试

--- webapp/src/test/java/com/xzixi/self/portal/webapp/ElasticsearchTests.java
+++ webapp/src/test/java/com/xzixi/self/portal/webapp/ElasticsearchTests.java
@@ -2,18 +2,14 @@ package com.xzixi.self.portal.webapp;
 
 import com.xzixi.self.portal.webapp.model.po.Article;
 import com.xzixi.self.portal.webapp.model.po.ArticleContent;
-import com.xzixi.self.portal.webapp.service.IArticleContentService;
-import com.xzixi.self.portal.webapp.service.IArticleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author 薛凌康
@@ -24,12 +20,6 @@ public class ElasticsearchTests {
 
     @Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
-    @Autowired
-    private IArticleService articleService;
-    @Autowired
-    private IArticleContentService articleContentService;
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
 
     @Test
     public void deleteArticleIndex() {
@@ -57,24 +47,6 @@ public class ElasticsearchTests {
         boolean putResult = elasticsearchTemplate.putMapping(ArticleContent.class);
         System.out.println(createResult);
         System.out.println(putResult);
-    }
-
-    @Test
-    public void testSaveArticleContent() {
-        ArticleContent content = new ArticleContent();
-        content.setArticleId(1);
-        content.setContent("作用在成员变量，标记为文档的字段，并指定字段映射属性");
-        articleContentService.save(content);
-    }
-
-    @Test
-    public void testRemove() {
-        redisTemplate.opsForList().remove("123", 1, 1);
-    }
-
-    @Test
-    public void testValue() {
-        redisTemplate.opsForValue().set("123::456", 2, 1800, TimeUnit.SECONDS);
     }
 
     @Test
