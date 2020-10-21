f18f39bffbb55d6f903007276348bc6f278b9345
xuelingkang <xuelingkang@163.com>
2020-10-21 20:42:28 +0800

    reset.sh

--- develop/reset.sh
+++ develop/reset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 删除开发环境初始化的容器
 
-docker stop mysql redis rabbitmq elasticsearch
-docker rm mysql redis rabbitmq elasticsearch
+docker stop mysql redis rabbitmq elasticsearch nacos
+docker rm mysql redis rabbitmq elasticsearch nacos
