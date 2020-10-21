3c5a647efd0da1fc406565283567915f281909f4
xuelingkang <xuelingkang@163.com>
2020-10-21 21:34:14 +0800

    fix

--- backend/src/main/java/com/xzixi/framework/backend/BackendCodeGenerator.java
+++ backend/src/main/java/com/xzixi/framework/backend/BackendCodeGenerator.java
@@ -7,12 +7,12 @@ import com.xzixi.framework.boot.webmvc.generator.CodeGeneratorConfig;
  * @author xuelingkang
  * @date 2020-10-10
  */
-public class Generator {
+public class BackendCodeGenerator {
 
     public static void main(String[] args) {
         CodeGenerator generator = new CodeGenerator();
         CodeGeneratorConfig config = new CodeGeneratorConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/portal?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf8");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/portal_backend?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf8");
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
         config.setUsername("root");
         config.setPassword("root");
---
+++ sso/src/main/java/com/xzixi/framework/sso/SsoCodeGenerator.java
@@ -0,0 +1,32 @@
+package com.xzixi.framework.sso;
+
+import com.xzixi.framework.boot.webmvc.generator.CodeGenerator;
+import com.xzixi.framework.boot.webmvc.generator.CodeGeneratorConfig;
+
+/**
+ * @author xuelingkang
+ * @date 2020-10-21
+ */
+public class SsoCodeGenerator {
+
+    public static void main(String[] args) {
+        CodeGenerator generator = new CodeGenerator();
+        CodeGeneratorConfig config = new CodeGeneratorConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/portal_sso?serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf8");
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setUsername("root");
+        config.setPassword("root");
+        config.setTablePrefix("t_");
+        config.setTables(new String[]{"t_test"});
+        config.setAuthor("xuelingkang");
+        config.setBaseDir("/sso/src/main/java");
+        config.setEntityPackage("com.xzixi.framework.sso.model.po");
+        config.setMapperPackage("com.xzixi.framework.sso.mapper");
+        config.setDataPackage("com.xzixi.framework.sso.data");
+        config.setDataImplPackage("com.xzixi.framework.sso.data.impl");
+        config.setServicePackage("com.xzixi.framework.sso.service");
+        config.setServiceImplPackage("com.xzixi.framework.sso.service.impl");
+        config.setControllerPackage("com.xzixi.framework.sso.controller");
+        generator.execute(config);
+    }
+}
