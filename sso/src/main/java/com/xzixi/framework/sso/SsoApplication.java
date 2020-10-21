20569b982d66f39dbc1d761b5c53b4e3e4ceff80
xuelingkang <xuelingkang@163.com>
2020-10-21 21:00:29 +0800

    初始化sso项目

---
+++ sso/src/main/java/com/xzixi/framework/sso/SsoApplication.java
@@ -0,0 +1,28 @@
+package com.xzixi.framework.sso;
+
+import com.xzixi.framework.boot.webmvc.config.async.annotation.EnableFrameworkAsync;
+import com.xzixi.framework.boot.webmvc.config.cache.annotation.EnableFrameworkCache;
+import com.xzixi.framework.boot.webmvc.config.json.annotation.EnableFrameworkJson;
+import com.xzixi.framework.boot.webmvc.config.mybatis.annotation.EnableFrameworkMybatis;
+import com.xzixi.framework.boot.webmvc.config.validation.annotation.EnableFrameworkValidation;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuelingkang
+ * @date 2020-10-21
+ */
+@SpringBootApplication
+@EnableFrameworkAsync
+@EnableFrameworkCache
+@EnableFrameworkJson
+@EnableFrameworkMybatis
+@EnableFrameworkValidation
+@MapperScan(basePackages = "com.xzixi.framework.sso.mapper")
+public class SsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class, args);
+    }
+}
