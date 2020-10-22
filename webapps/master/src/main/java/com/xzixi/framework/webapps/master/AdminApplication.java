4407c00c885102bb723a727268de16f41966fc12
xuelingkang <xuelingkang@163.com>
2020-10-22 23:46:07 +0800

    初始化master服务

---
+++ webapps/master/src/main/java/com/xzixi/framework/webapps/master/AdminApplication.java
@@ -0,0 +1,20 @@
+package com.xzixi.framework.webapps.master;
+
+import com.xzixi.framework.boot.webmvc.config.json.annotation.EnableFrameworkJson;
+import com.xzixi.framework.boot.webmvc.config.validation.annotation.EnableFrameworkValidation;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuelingkang
+ * @date 2020-10-22
+ */
+@SpringBootApplication
+@EnableFrameworkJson
+@EnableFrameworkValidation
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
