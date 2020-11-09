a065e3751c6bf75a5f752064bf3d63f6408805b8
xuelingkang <xuelingkang@163.com>
2020-11-09 20:01:40 +0800

    优化RedisPipelineService

--- boot/redis/src/main/java/com/xzixi/framework/boot/redis/service/impl/RedisPipelineService.java
+++ boot/redis/src/main/java/com/xzixi/framework/boot/redis/service/impl/RedisPipelineService.java
@@ -17,6 +17,7 @@
 
 package com.xzixi.framework.boot.redis.service.impl;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.RedisStringCommands;
@@ -26,6 +27,7 @@ import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -38,6 +40,8 @@ import java.util.stream.Collectors;
 @Service
 public class RedisPipelineService {
 
+    private static final int DEFAULT_BATCH_SIZE = 100;
+
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
     @Autowired
@@ -47,34 +51,48 @@ public class RedisPipelineService {
 
     /**
      * 使用pipeline批量执行redis查询
+     * <p>每100个执行一次
      *
      * @param keys key有序集合
      * @return values，和入ids顺序和长度，查询不到的用null占位
      */
-    @SuppressWarnings("unchecked")
     public <T> List<T> get(Collection<String> keys) {
-        List<Object> values = redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
-            for (String key : keys) {
-                byte[] keyBytes = stringRedisSerializer.serialize(key);
-                assert keyBytes != null;
-                connection.get(keyBytes);
-            }
-            return null;
-        });
-        return values.stream().map(value -> (T) value).collect(Collectors.toList());
+        return get(keys, DEFAULT_BATCH_SIZE);
+    }
+
+    /**
+     * 使用pipeline批量执行redis查询
+     *
+     * @param keys key有序集合
+     * @param batchSize 每批次执行多少个
+     * @return values，和入ids顺序和长度，查询不到的用null占位
+     */
+    @SuppressWarnings("unchecked")
+    public <T> List<T> get(Collection<String> keys, int batchSize) {
+        return Lists.partition(new ArrayList<>(keys), batchSize).stream()
+                .map(partition -> (List<T>) redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
+                    for (String key : partition) {
+                        byte[] keyBytes = stringRedisSerializer.serialize(key);
+                        assert keyBytes != null;
+                        connection.get(keyBytes);
+                    }
+                    return null;
+                }))
+                .flatMap(Collection::stream).collect(Collectors.toList());
     }
 
     /**
      * 使用pipeline批量执行redis保存
-     * <p>key已存在时会覆盖
+     * <p>每100个执行一次
      *
      * @param pairs 键值对集合
      * @param expire 过期时间
      * @param timeUnit 时间单位
+     * @param setOption {@link RedisStringCommands.SetOption}
      * @param <P> Pair&lt;String, ?>
      */
-    public <P extends Pair<String, ?>> void set(Collection<P> pairs, long expire, TimeUnit timeUnit) {
-        set(pairs, expire, timeUnit, RedisStringCommands.SetOption.UPSERT);
+    public <P extends Pair<String, ?>> void set(Collection<P> pairs, long expire, TimeUnit timeUnit, RedisStringCommands.SetOption setOption) {
+        set(pairs, expire, timeUnit, setOption, DEFAULT_BATCH_SIZE);
     }
 
     /**
@@ -84,35 +102,49 @@ public class RedisPipelineService {
      * @param expire 过期时间
      * @param timeUnit 时间单位
      * @param setOption {@link RedisStringCommands.SetOption}
+     * @param batchSize 每批次执行多少个
      * @param <P> Pair&lt;String, ?>
      */
-    public <P extends Pair<String, ?>> void set(Collection<P> pairs, long expire, TimeUnit timeUnit, RedisStringCommands.SetOption setOption) {
-        redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
-            for (P pair : pairs) {
-                byte[] keyBytes = stringRedisSerializer.serialize(pair.getKey());
-                byte[] valueBytes = objectRedisSerializer.serialize(pair.getValue());
-                Expiration expiration = Expiration.from(expire, timeUnit);
-                assert keyBytes != null;
-                assert valueBytes != null;
-                connection.set(keyBytes, valueBytes, expiration, setOption);
-            }
-            return null;
-        });
+    public <P extends Pair<String, ?>> void set(Collection<P> pairs, long expire, TimeUnit timeUnit, RedisStringCommands.SetOption setOption, int batchSize) {
+        Lists.partition(new ArrayList<>(pairs), batchSize)
+                .forEach(partition -> redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
+                    for (P pair : partition) {
+                        byte[] keyBytes = stringRedisSerializer.serialize(pair.getKey());
+                        byte[] valueBytes = objectRedisSerializer.serialize(pair.getValue());
+                        Expiration expiration = Expiration.from(expire, timeUnit);
+                        assert keyBytes != null;
+                        assert valueBytes != null;
+                        connection.set(keyBytes, valueBytes, expiration, setOption);
+                    }
+                    return null;
+                }));
+    }
+
+    /**
+     * 使用pipeline批量执行redis删除
+     * <p>每100个执行一次
+     *
+     * @param keys key集合
+     */
+    public void del(Collection<String> keys) {
+        del(keys, DEFAULT_BATCH_SIZE);
     }
 
     /**
      * 使用pipeline批量执行redis删除
      *
      * @param keys key集合
+     * @param batchSize 每批次执行多少个
      */
-    public void del(Collection<String> keys) {
-        redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
-            for (String key : keys) {
-                byte[] keyBytes = stringRedisSerializer.serialize(key);
-                assert keyBytes != null;
-                connection.del(keyBytes);
-            }
-            return null;
-        });
+    public void del(Collection<String> keys, int batchSize) {
+        Lists.partition(new ArrayList<>(keys), batchSize)
+                .forEach(partition -> redisTemplate.executePipelined((RedisCallback<Object>) connection -> {
+                    for (String key : partition) {
+                        byte[] keyBytes = stringRedisSerializer.serialize(key);
+                        assert keyBytes != null;
+                        connection.del(keyBytes);
+                    }
+                    return null;
+                }));
     }
 }
