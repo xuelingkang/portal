7faf971a169e31d4164e3f29be5cc5714d7e2e3a
xuelingkang <xuelingkang@163.com>
2021-01-07 00:25:05 +0800

    TokenFilter

--- webapps/sso/sso-client/src/main/java/com/xzixi/framework/webapps/sso/client/config/security/TokenFilter.java
+++ webapps/sso/sso-client/src/main/java/com/xzixi/framework/webapps/sso/client/config/security/TokenFilter.java
@@ -19,6 +19,20 @@
 
 package com.xzixi.framework.webapps.sso.client.config.security;
 
+import com.xzixi.framework.boot.core.model.Result;
+import com.xzixi.framework.webapps.common.model.vo.UserVO;
+import com.xzixi.framework.webapps.remote.service.RemoteUserService;
+import com.xzixi.framework.webapps.sso.common.constant.SsoConstant;
+import com.xzixi.framework.webapps.sso.common.constant.TokenConstant;
+import com.xzixi.framework.webapps.sso.common.model.AppAccessTokenValue;
+import com.xzixi.framework.webapps.sso.common.model.UserDetailsImpl;
+import com.xzixi.framework.webapps.sso.common.util.WebUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -35,7 +49,39 @@ import java.io.IOException;
 @Component
 public class TokenFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+    @Autowired
+    private RemoteUserService remoteUserService;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 获取请求头或请求参数上的accessToken
+        String accessToken = WebUtils.getHeaderOrParameter(request, SsoConstant.ACCESS_TOKEN_HEADER_NAME, SsoConstant.ACCESS_TOKEN_NAME);
+        // redis key
+        String key = String.format(TokenConstant.APP_ACCESS_TOKEN_KEY_TEMPLATE, accessToken);
+        // 查询redis
+        AppAccessTokenValue accessTokenValue = (AppAccessTokenValue) redisTemplate.boundValueOps(key).get();
+        // 设置当前登录人信息
+        if (accessTokenValue != null) {
+            UserVO userVO = remoteUserService.getById(accessTokenValue.getUserId()).getData();
+            if (userVO != null) {
+                UserDetails userDetails = new UserDetailsImpl(userVO);
+                if (!userDetails.isEnabled()) {
+                    Result<?> result = new Result<>(HttpStatus.UNAUTHORIZED.value(), 0, "账户未激活！");
+                    WebUtils.printJson(response, result);
+                    return;
+                }
+                if (!userDetails.isAccountNonLocked()) {
+                    Result<?> result = new Result<>(HttpStatus.UNAUTHORIZED.value(), 0, "账户已被锁定！");
+                    WebUtils.printJson(response, result);
+                    return;
+                }
+                UsernamePasswordAuthenticationToken authentication
+                        = new UsernamePasswordAuthenticationToken(userDetails, accessToken, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 }
