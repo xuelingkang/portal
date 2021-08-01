b3993374d259a49b3b4ef3669a77348435bb636c
xuelingkang <xuelingkang@163.com>
2021-08-01 16:00:26 +0800

    Pref: docker打包优化

---
+++ webapps/gateway/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [ "$(id -u)" -eq 0 ]; then
+  if ! grep -E "^portal" /etc/group > /dev/null 2>&1; then
+    groupadd portal
+  fi
+
+  if ! grep -E "^portal" /etc/passwd > /dev/null 2>&1; then
+    useradd -d /portal -g portal portal
+  fi
+
+  uid=$(id -u portal)
+  gid=$(id -g portal)
+
+  chown -R "${uid}:${gid}" /app
+  chown -R "${uid}:${gid}" /var/log/portal/gateway
+  chmod 777 /tmp
+
+  exec gosu "${uid}:${gid}" "$@"
+fi
+
+exec "$@"
+
